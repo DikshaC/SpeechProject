f28fb7c7c0135246d1b5ae60c4f2fa3e99be0886
dchhabra1995@gmail.com <gchhabra1995>
2017-05-04 23:32:02 +0530

    Added files

---
+++ app/src/main/java/com/example/hp/speechproject/NoteFrequency.java
@@ -0,0 +1,15 @@
+package com.example.hp.speechproject;
+
+/**
+ * Created by HP on 03-May-17.
+ */
+public class NoteFrequency {
+   double frequency;
+    String note;
+
+    public NoteFrequency(double frequency,String note)
+    {
+        this.frequency=frequency;
+        this.note=note;
+    }
+}
